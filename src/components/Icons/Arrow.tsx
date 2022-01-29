82e50301b26d658f204128f780971b18c039d38c
Emily Sun <esun0703@gmail.com>
2022-01-29 17:08:14 -0300

    udpate

--- src/components/Icons/Arrow.tsx
+++ src/components/Icons/Arrow.tsx
@@ -1,14 +1,14 @@
+import './Arrow.scss'
 export const Arrow = () => (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         width="35" height="35" 
         viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
+        fill="none"  
         strokeWidth="2" 
         strokeLinecap="round" 
         strokeLinejoin="round" 
-        className="feather feather-arrow-right"
+        className="arrow-right"
     >
         <line x1="5" y1="12" x2="19" y2="12"/>
         <polyline points="12 5 19 12 12 19"/>
