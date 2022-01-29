2b8bc1bebfa36dcef069dc6c2aaeeb0f5d208860
Emily Sun <esun0703@gmail.com>
2022-01-29 16:11:37 -0300

    commiting current work, attempting to hook up muse api, however waiting for response regarding not having job title exposure in api

--- src/contexts/JobContext.tsx
+++ src/contexts/JobContext.tsx
@@ -1,4 +1,5 @@
 import React, { useState, createContext, FC} from 'react';
+import Axios from 'axios';
 
 interface SingleJob {
     title: string,
@@ -13,10 +14,18 @@ export const Context = createContext<ContextDefault | null>(null);
 export const JobSearchProvider: FC = ({children}) => {
     // const [jobTitle, setJobTitle] = useState('');
     const [jobsList, setJobsList] = useState<SingleJob[]>([]);
+    const [jobSearchPage, setjobSearchPage] = useState<number>(0);
 
     const searchJobTitle = async (jobTitle : string) => {
-        const result = [{title: 'this is a job for now'}, {title: 'this is another job'}, {title: 'this is a job for now'}, {title: 'this is another job'}]
-        setJobsList(result);
+        try {
+            const {data} = await Axios.get(`https://www.themuse.com/api/public/jobs?category=${'Accounting'}&page=${jobSearchPage}`);
+            console.log({data})
+            const result = [{title: 'this is a job for now'}, {title: 'this is another job'}, {title: 'this is a job for now'}, {title: 'this is another job'}]
+            setJobsList(result);
+            setjobSearchPage(jobSearchPage + 1)
+        } catch(e) {
+            console.error(e)
+        }
     }
     
     return (
